dccde55b4a72e2b47ebf39715c40ac340096e1a5
Sidney Sousa <sidney.sousa@ifms.edu.br>
2019-03-15 20:22:02 -0400

    Adicionando navegação

---
+++ gestao-estoque/src/components/Materiais.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Materiais extends Component {
+
+    render() {
+        return (
+            <div>Materiais</div>
+        );
+    }
+}
+
+export default Materiais;

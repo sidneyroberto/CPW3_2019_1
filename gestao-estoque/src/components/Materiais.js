8fc8df395f3f3065d08949ad8dc9e56facaaafb9
Sidney Sousa <sidney.sousa@ifms.edu.br>
2019-03-29 20:18:39 -0400

    Incrementando o projeto gestao-estoque

--- gestao-estoque/src/components/Materiais.js
+++ gestao-estoque/src/components/Materiais.js
@@ -2,9 +2,81 @@ import React, { Component } from 'react';
 
 class Materiais extends Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            descricao: '',
+            marca: '',
+            categoria: ''
+        };
+
+        /*
+         * Dá acesso a tudo o que foi definido
+         * dentro do construtor para o método
+         * aoAlterarValor.
+         */
+        this.aoAlterarValor =
+            this.aoAlterarValor.bind(this);
+    }
+
+    aoAlterarValor(evento) {
+        const valor = evento.target.value;
+        const nome = evento.target.name;
+        this.setState({ [nome]: valor });
+    }
+
     render() {
         return (
-            <div>Materiais</div>
+            <div>
+                <h3>Novo material</h3>
+
+                <div className="row">
+                    <div className="col-md-6">
+                        <form>
+                            <div className="form-group">
+                                <label>Descrição</label>
+                                <input
+                                    onChange={this.aoAlterarValor}
+                                    value={this.state.descricao}
+                                    name="descricao"
+                                    className="form-control"
+                                    required
+                                    type="text" />
+                            </div>
+                            <div className="form-group">
+                                <label>Marca</label>
+                                <input
+                                    onChange={this.aoAlterarValor}
+                                    value={this.state.marca}
+                                    name="marca"
+                                    className="form-control"
+                                    required
+                                    type="text" />
+                            </div>
+                            <div className="form-group">
+                                <label>Categoria</label>
+                                <select
+                                    onChange={this.aoAlterarValor}
+                                    value={this.state.categoria}
+                                    name="categoria"
+                                    required
+                                    className="form-control">
+                                    <option>Eletrônicos</option>
+                                    <option>Eletrodomésticos</option>
+                                    <option>Vestuário</option>
+                                    <option>Higiene Pessoal</option>
+                                </select>
+                            </div>
+
+                            <input
+                                className="btn btn-primary"
+                                type="submit"
+                                value="Salvar" />
+                        </form>
+                    </div>
+                </div>
+            </div>
         );
     }
 }

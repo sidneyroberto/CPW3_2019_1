641f35ab48184e12aaa153cb2ce309349f138e32
Sidney Sousa <sidney.sousa@ifms.edu.br>
2019-04-05 20:24:47 -0400

    incrementando o projeto gestao-estoque

--- gestao-estoque/src/components/Materiais.js
+++ gestao-estoque/src/components/Materiais.js
@@ -7,9 +7,11 @@ class Materiais extends Component {
         super();
 
         this.state = {
-            descricao: '',
-            marca: '',
-            categoria: ''
+            material: {
+                descricao: '',
+                marca: '',
+                categoria: 'Eletrônicos'
+            }
         };
 
         /*
@@ -25,23 +27,64 @@ class Materiais extends Component {
     salvar(evento) {
         evento.preventDefault();
 
-        let material = this.state;
+        let material = this.state.material;
 
         database
             .ref('materiais')
-            .push(material);
+            .push(material)
+            .then(
+                () => {
+                    let material = {
+                        descricao: '',
+                        marca: '',
+                        categoria: 'Eletrônicos'
+                    };
+                    this.setState({ material });
+                },
+                erro => console.log(erro)
+            )
+            .catch(erro => console.log(erro));
     }
 
     aoAlterarValor(evento) {
         const valor = evento.target.value;
         const nome = evento.target.name;
-        this.setState({ [nome]: valor });
+
+        // Pega a propriedade "material" do state do componente
+        let material = { ...this.state.material };
+        /*
+         * Altera a subpropriedade de "material" cujo nome
+         * corresponde ao nome do campo que está sendo alterado
+         */
+        material[nome] = valor;
+
+        this.setState({ material });
     }
 
     render() {
         return (
             <div>
                 <h3>Novo material</h3>
+
+                <br />
+
+                <div className="row">
+                    <div className="col">
+                        <span className="alert alert-success">
+                            Material salvo
+                        </span>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <span className="alert alert-danger">
+                            Ocorreu um problema ao tentar salvar o material
+                        </span>
+                    </div>
+                </div>
+
+                <br />
 
                 <div className="row">
                     <div className="col-md-6">
@@ -50,7 +93,7 @@ class Materiais extends Component {
                                 <label>Descrição</label>
                                 <input
                                     onChange={this.aoAlterarValor}
-                                    value={this.state.descricao}
+                                    value={this.state.material.descricao}
                                     name="descricao"
                                     className="form-control"
                                     required
@@ -60,7 +103,7 @@ class Materiais extends Component {
                                 <label>Marca</label>
                                 <input
                                     onChange={this.aoAlterarValor}
-                                    value={this.state.marca}
+                                    value={this.state.material.marca}
                                     name="marca"
                                     className="form-control"
                                     required
@@ -70,14 +113,14 @@ class Materiais extends Component {
                                 <label>Categoria</label>
                                 <select
                                     onChange={this.aoAlterarValor}
-                                    value={this.state.categoria}
+                                    value={this.state.material.categoria}
                                     name="categoria"
                                     required
                                     className="form-control">
-                                    <option>Eletrônicos</option>
-                                    <option>Eletrodomésticos</option>
-                                    <option>Vestuário</option>
-                                    <option>Higiene Pessoal</option>
+                                    <option value="Eletrônicos">Eletrônicos</option>
+                                    <option value="Eletrodomésticos">Eletrodomésticos</option>
+                                    <option value="Vestuário">Vestuário</option>
+                                    <option value="Higiene Pessoal">Higiene Pessoal</option>
                                 </select>
                             </div>
 
